2aaa258c2c42045900821c038aab70ba6dd9d284
Sourabh Daptardar <saurabh.daptardar@gmail.com>
2018-02-08 16:21:08 +0530

    Refactored embedding code into a function

--- train.py
+++ train.py
@@ -31,6 +31,42 @@ import random
 from datasets.folder_ext import ImageFolderWithFilenames
 from samplers.triplet_sampler import TripletSampler
 
+
+def embed(args, dset, loader, model_fe):
+    """embeds images into feature vectors using current model."""
+
+    embedding_id = [np.zeros((0, args.feature_dim), dtype=np.float32) for c in range(args.num_identities)]
+    embedding_neg_id = [np.zeros((0, args.feature_dim), dtype=np.float32) for c in range(args.num_identities)]
+    index_id = [[] for c in range(args.num_identities)]
+    index_neg_id = [[] for c in range(args.num_identities)]
+    embedding = np.zeros((len(dset), args.feature_dim), dtype=np.float32)
+    img_fnames = []
+    for i, mb in enumerate(loader):
+        img, target, iname = mb
+        fv = model_fe(Variable(img.cuda()))
+        embedding[i*args.batch_size:(i+1)*args.batch_size,:] = fv.clone().data.cpu().numpy()
+        img_fnames.extend(iname)
+        bs = fv.size(0)
+        for j in range(bs):
+            # embedding_id[target[j]] = np.vstack((embedding_id[target[j]], fv[j,:].clone().data.cpu().numpy()))
+            for c in range(args.num_identities):
+                if c != target[j]:
+                    embedding_neg_id[c] = np.vstack((embedding_neg_id[c], fv[j,:].clone().data.cpu().numpy()))
+                    index_neg_id[c].append(i * args.batch_size + j)
+                else:
+                    embedding_id[target[j]] = np.vstack((embedding_id[target[j]], fv[j,:].clone().data.cpu().numpy()))
+                    index_id[c].append(i * args.batch_size + j)
+
+        fv = None
+        img = None
+        target = None
+        iname = None
+        mb = None
+        gc.collect()
+    # joblib.dump(embedding, args.emb_fv_file)
+    return embedding, img_fnames, embedding_id, index_id, embedding_neg_id, index_neg_id
+
+
 def main(args):
 
     cudnn.benchmark = True
@@ -113,72 +149,15 @@ def main(args):
 
     for epoch in range(args.num_epochs):
 
+        logging.error('Epoch {:04d}'.format(epoch))
+
         ## STEP 1 : EMBED IMAGES INTO FEATURE VECTORS USING CURRENT MODEL
 
-        logging.error('Epoch {:04d}'.format(epoch))
-        embedding_id = [np.zeros((0, args.feature_dim), dtype=np.float32) for c in range(args.num_identities)]
-        embedding_neg_id = [np.zeros((0, args.feature_dim), dtype=np.float32) for c in range(args.num_identities)]
-        index_id = [[] for c in range(args.num_identities)]
-        index_neg_id = [[] for c in range(args.num_identities)]
-        embedding = np.zeros((len(train_dset), args.feature_dim), dtype=np.float32)
-        img_fnames = []
-        for i, mb in enumerate(train_loader):
-            img, target, iname = mb
-            fv = model_fe(Variable(img.cuda()))
-            embedding[i*args.batch_size:(i+1)*args.batch_size,:] = fv.clone().data.cpu().numpy()
-            img_fnames.extend(iname)
-            bs = fv.size(0)
-            for j in range(bs):
-                # embedding_id[target[j]] = np.vstack((embedding_id[target[j]], fv[j,:].clone().data.cpu().numpy()))
-                for c in range(args.num_identities):
-                    if c != target[j]:
-                        embedding_neg_id[c] = np.vstack((embedding_neg_id[c], fv[j,:].clone().data.cpu().numpy()))
-                        index_neg_id[c].append(i * args.batch_size + j)
-                    else:
-                        embedding_id[target[j]] = np.vstack((embedding_id[target[j]], fv[j,:].clone().data.cpu().numpy()))
-                        index_id[c].append(i * args.batch_size + j)
-
-            fv = None
-            img = None
-            target = None
-            iname = None
-            mb = None
-            gc.collect()
-        # joblib.dump(embedding, args.emb_fv_file)
-
-
-        ## STEP 2 : COMPUTE (APPROXIMATE) NEAREST NEIGHBOURS 
-
-        ## Hard triplet mining with FLANN
-        ## Disabled as FAISS is used instead
-        # hard = []    
-        # ann = [flann.FLANN() for i in range(args.num_identities)]
-        # ann_neg = [flann.FLANN() for i in range(args.num_identities)]
-        # for k in range(args.num_identities):
-        #     args.ann_file = os.path.join(args.ckpt_dir, 'ann_{:s}_{:s}_{:04d}.npz'.format(args.dset_name, args.arch, k))
-        #     ann_params = ann[k].build_index(embedding_id[k], algorithm='autotuned', target_precision=0.9, log_level='error')
-        #     ann_index, ann_dist = ann[k].nn_index(embedding_id[k], embedding_id[k].shape[0], checks=ann_params['checks'])
-        #     ann_neg_params = ann_neg[k].build_index(embedding_neg_id[k], algorithm='autotuned', target_precision=0.9, log_level='error')
-        #     ann_neg_index, ann_neg_dist = ann_neg[k].nn_index(embedding_id[k], args.num_neighbors, checks=ann_neg_params['checks'])
-        #     for a_ in range(ann_index.shape[0]):
-        #         for p_ctr in range(args.num_neighbors):
-        #             p_ = int(ann_index.shape[1]) - 1  - p_ctr
-        #             a = index_id[k][a_]
-        #             for n_ in range(args.num_neighbors):
-        #                 p = index_id[k][ann_index[a_, p_]]
-        #                 n = index_neg_id[k][ann_neg_index[a_, n_]]
-        #                 if ann_dist[a_, p_] - ann_neg_dist[a_, n_] + args.margin >= 0:  # hard example: violates margin 
-        #                     hard.append((a, p, n))
-        #     print('#Tuples: ', len(hard))
-        #     print(hard)
-        #     joblib.dump({'ann_index': ann_index, 'ann_dist': ann_dist, 'ann_neg_index': ann_neg_index, 'ann_neg_dist': ann_neg_dist}, args.ann_file)
-        #     ann_params = None
-        #     ann_index = None
-        #     ann_dist = None
-        # ann = None
-
-
-        ## STEP 3: HARD NEGATIVE TRIPLET MINING
+        embedding, img_fnames, embedding_id, index_id, embedding_neg_id, index_neg_id = \
+            embed(args, train_dset, train_loader, model_fe)
+        
+        
+        ## STEP 2: HARD NEGATIVE TRIPLET MINING
 
         hard = []
         res = faiss.StandardGpuResources()
